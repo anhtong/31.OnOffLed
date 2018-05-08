e14a3806434eb094f0728b86b0c2da2b5709b3be
Anh Tong <tvhuy2008@gmail.com>
2018-05-08 22:31:22 +0700

    Initial commit

---
+++ app/src/main/java/com/anhtong8x/a31onoffled/ControlLedActivity.java
@@ -0,0 +1,274 @@
+package com.anhtong8x.a31onoffled;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ControlLedActivity extends AppCompatActivity {
+
+	// Bat tat 1 led 
+	// File ardunio o duoi
+
+
+    Button btnOn, btnOff, btnDis;
+    SeekBar brightness;
+    TextView lumn;
+    String address = null;
+    private ProgressDialog progress;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    //SPP UUID. Look for it
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent newint = getIntent();
+        address = newint.getStringExtra(MainActivity.EXTRA_ADDRESS);
+
+        //view of the ledControl
+        setContentView(R.layout.activity_control_led);
+
+        //call the widgtes
+        btnOn = (Button)findViewById(R.id.button2);
+        btnOff = (Button)findViewById(R.id.button3);
+        btnDis = (Button)findViewById(R.id.button4);
+        brightness = (SeekBar)findViewById(R.id.seekBar);
+        lumn = (TextView)findViewById(R.id.lumn);
+
+        new ConnectBT().execute(); //Call the class to connect
+
+        //commands to be sent to bluetooth
+        btnOn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                turnOnLed();      //method to turn on
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                turnOffLed();   //method to turn off
+            }
+        });
+
+        btnDis.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Disconnect(); //close connection
+            }
+        });
+
+        brightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser==true)
+                {
+                    lumn.setText(String.valueOf(progress));
+                    try
+                    {
+                        btSocket.getOutputStream().write(String.valueOf(progress).getBytes());
+                    }
+                    catch (IOException e)
+                    {
+
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    //==========
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e)
+            { msg("Error");}
+        }
+        finish(); //return to the first layout
+
+    }
+
+    private void turnOffLed()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("TF".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private void turnOnLed()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                btSocket.getOutputStream().write("TO".toString().getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    // fast way to call Toast
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(ControlLedActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            }
+            else
+            {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+
+    //
+}
+
+/*
+#include <SoftwareSerial.h>
+#define LED 9
+char _char;
+String _string;
+byte _byte;
+
+void setup() {
+  //mySerial.begin(9600);
+  Serial.begin(9600);
+  pinMode( LED , OUTPUT );
+}
+
+void loop() {
+
+  if( Serial.available() >0 ) { _string = ""; }
+
+  while(Serial.available() > 0 ){
+      _char = ((byte) Serial.read());
+      if ( _char == ':'){
+        break;
+      }else{
+        _string += _char;
+      }
+      delay (1);              
+  }
+
+  if ( _string == "TO" ){
+    LedOn ();  
+  }
+
+  if ( _string == "TF" ){
+    LedOff ();
+  }
+    
+  if ((_string.toInt()>=0)&&(_string.toInt()<=255))
+  {
+    ChangeLed ();
+  }  
+}
+
+// Change led
+void ChangeLed () {
+  analogWrite( LED , _string.toInt() );      
+  Serial.println(_string);
+  delay(10); 
+}
+
+void LedOn () {  
+  analogWrite( LED , 10 );  
+}
+
+void LedOff () {
+  analogWrite (LED, LOW );  
+  delay (500);
+}
+
+*/
